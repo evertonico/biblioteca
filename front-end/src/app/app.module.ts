51d6334584856f4d83f087780c740e85942e26f2
Giovani Campelo <giovanicaf@gmail.com>
2022-01-10 19:41:32 -0300

    Concuindo servico de autenticacao

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { HomeModule } from './home/home.module';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front-end/src/app/home/login/login.component.ts
+++ front-end/src/app/home/login/login.component.ts
@@ -1,3 +1,4 @@
+import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  usuario='';
+  senha='';
+
+  constructor(private authService: AutenticacaoService) { }
 
   ngOnInit(): void {
+  }
+  login(){
+    this.authService.autenticar(this.usuario, this.senha).subscribe(() => {
+      console.log('Autenticado com sucesso');
+    },(error) => {
+      alert('Usuário ou senha inválido');
+      console.log(error);
+    }
+    );
   }
 
 }
