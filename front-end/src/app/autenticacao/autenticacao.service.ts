b36912346df35bc2584e0831db0f0315bb8161ef
Giovani Campelo <giovanicaf@gmail.com>
2022-01-10 19:08:43 -0300

    Configurando servico de autenticacao

--- front-end/src/app/autenticacao/autenticacao.service.ts
+++ front-end/src/app/autenticacao/autenticacao.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AutenticacaoService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+  autenticar(usuario:string, senha:string):Observable<any>{
+    return this.httpClient.post('http://localhost:3000/user/login', {
+      userName: usuario,
+      password: senha,
+    })
+  }
 }
