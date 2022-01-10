7595d1476a7507bd76287ae911d770aabc7ffb87
Giovani Campelo <giovanicaf@gmail.com>
2022-01-10 20:21:53 -0300

    Adicionando validacao do formulario de login

--- front-end/src/app/componentes/mensagem/mensagem.module.ts
+++ front-end/src/app/componentes/mensagem/mensagem.module.ts
@@ -10,6 +10,7 @@ import { MensagemComponent } from './mensagem.component';
   ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports:[MensagemComponent],
 })
 export class MensagemModule { }
--- front-end/src/app/home/home.module.ts
+++ front-end/src/app/home/home.module.ts
@@ -5,6 +5,8 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
+import { MensagemComponent } from '../componentes/mensagem/mensagem.component';
+import { MensagemModule } from '../componentes/mensagem/mensagem.module';
 
 
 @NgModule({
@@ -15,7 +17,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     HomeRoutingModule,
-    FormsModule
+    FormsModule,
+    MensagemModule
   ],
   exports: [HomeComponent],
 })
