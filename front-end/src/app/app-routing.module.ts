28cf72a405dd60532ebbc147951f03a77836d369
Giovani Campelo <giovanicaf@gmail.com>
2022-01-10 19:54:41 -0300

    Redirecionando se logado para galeria

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then((m)=> m.HomeModule),
+  },
+  {
+    path:'galeria',
+    loadChildren: () => import('./galeria/galeria.module').then((m) => m.GaleriaModule),
   }
 ];
 
--- front-end/src/app/galeria/galeria-routing.module.ts
+++ front-end/src/app/galeria/galeria-routing.module.ts
@@ -1,7 +1,13 @@
+import { ListaComponent } from './lista/lista.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: ListaComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
