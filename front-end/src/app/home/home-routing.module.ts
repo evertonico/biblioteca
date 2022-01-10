a1fd14423f0c155aaeb29b94e1bb70e495a01230
Giovani Campelo <giovanicaf@gmail.com>
2022-01-10 18:58:20 -0300

    Criando componente login

--- front-end/src/app/home/home-routing.module.ts
+++ front-end/src/app/home/home-routing.module.ts
@@ -1,3 +1,4 @@
+import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -6,7 +7,13 @@ const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-  }
+    children: [
+      {
+        path: '',
+        component: LoginComponent,
+      }
+    ]
+  },
 ];
 
 @NgModule({
