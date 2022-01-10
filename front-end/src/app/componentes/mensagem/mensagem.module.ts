05f6a5c9f2eb22af97cc18483890fc0ee18a8f34
Giovani Campelo <giovanicaf@gmail.com>
2022-01-10 19:56:51 -0300

    Criando modulo e componente para mensagens

---
+++ front-end/src/app/componentes/mensagem/mensagem.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MensagemComponent } from './mensagem.component';
+
+
+
+@NgModule({
+  declarations: [
+    MensagemComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MensagemModule { }
