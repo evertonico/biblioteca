13304bed23b9293bbf3d6003a33e6a936ef4fcc9
Giovani Campelo <giovanicaf@gmail.com>
2022-01-10 23:15:10 -0300

    Criando modulos e componentes de cabecalho

---
+++ front-end/src/app/componentes/cabecalho/cabecalho.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CabecalhoComponent } from './cabecalho.component';
+
+
+
+@NgModule({
+  declarations: [
+    CabecalhoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CabecalhoModule { }
