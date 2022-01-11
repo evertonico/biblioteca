9c2ce2a2ebb8f42ab30be3e69eb735f3c70e0f8b
Daniel Aguiar da Nóbrega <danielnobrega88@gmail.com>
2022-01-11 15:43:05 -0300

    Criação da Estrutura de Usuário

--- back/src/main/java/com/example/biblioteca/model/repository/UsuarioRepository.java
+++ back/src/main/java/com/example/biblioteca/model/repository/UsuarioRepository.java
@@ -1,9 +1,15 @@
 package com.example.biblioteca.model.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.biblioteca.model.entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	
+	boolean existsByEmail(String email);
+	
+	Optional<Usuario> findByLogin(String login);
 	
 }
